f2e981f836724e387fce51d3069f503f491de219
Christian Kouris <ckouris@ucsd.edu>
2019-10-12 16:05:47 -0700

    created the build and buildSubTree methods

--- src/kdt/KDT.hpp
+++ src/kdt/KDT.hpp
@@ -55,8 +55,29 @@ class KDT {
     /** Destructor of KD tree */
     virtual ~KDT() { deleteAll(root); }
 
-    /** TODO */
-    void build(vector<Point>& points) {}
+    /** builds a balanced KDT tree from a set of points.  */
+    void build(vector<Point>& points) {
+
+        //check to see if vector is empty and return
+        if( points.size() == 0 ) {
+            return;
+        }
+        //set member variables for the root Node
+        isize = 1; iheight = 0; curDim = 0; 
+        //define new variables to help organize logic
+        unsigned int start = 0; unsigned int end = points.size();
+        unsigned int median = (start+end)/2;
+
+        //sort the points vector
+        std::sort(points.begin(), points.end(), CompareValueAt(curDim));
+        //set the median of the sorted list as the rood of the KDT
+        root = new KDNode( points[median] );
+        
+        //call the helper method for the root's children
+        root->left = buildSubtree( points, start, median, curDim + 1, 1 )
+        root->right = buildSubtree( points, median + 1, end, curDim + 1, 1 )
+
+    }
 
     /** TODO */
     Point* findNearestNeighbor(Point& queryPoint) { return nullptr; }
@@ -73,10 +94,42 @@ class KDT {
     int height() const { return iheight; }
 
   private:
-    /** TODO */
+    /** Recursive function to build the balanced KD Tree level by level.
+     *  Each level takes a section of the vector, sorts it, finds a new
+     *  median root, and repeats with the subvectors
+     *  Parameter: points - a vector of points to serve as the KDT data
+     *  Parameter: start - the inclusive start index of the subvector
+     *  Parameter: end - the exclusive end index of the subvector
+     *  Parameter: curDim - The current dimension of the recursion level
+     *  Parameter: height - the current height of the level
+     */
     KDNode* buildSubtree(vector<Point>& points, unsigned int start,
                          unsigned int end, unsigned int curDim, int height) {
-        return nullptr;
+        if( start == end) {
+            return nullptr;
+        }
+        //sort this part of the list
+        std::sort(points.begin() + start, points.begin() + end, 
+                  CompareValueAt(curDim % points.begin().curDim) );
+
+        //allocate memory for the median Node
+        unsigned int median = (start+end)/2;
+        KDTNode * medNode = new KDTNode( points[median] );
+
+        //recurse left and right
+        medNode->left = buildSubtree( points, start, median, 
+                                      curDim + 1, height + 1 );
+        medNode->right = buildSubtree( points, median + 1, end,
+                                       curDim + 1, height + 1 );
+        
+        //increment size and check to see if height is taller than current
+        isize++;
+        if( iheight < height ) {
+            iheight = height;
+        }
+        //return the Node
+        return medNode;
+        
     }
 
     /** TODO */
